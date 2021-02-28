0dfc0d633a945a4bd2c3a797b2c991c487a6e464
wayne <juchuang123@gmail.com>
2021-02-28 17:00:52 +0900

    index.jsの作成

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
-import HomePage from '../components/HomePage';
 import PageNotFound from '../components/PageNotFound';
 
 const AppRouter = () => {
@@ -11,7 +10,7 @@ const AppRouter = () => {
       <div className="container">
         <Header />
         <Switch>
-          <Route path="/" content={HomePage} exact={true} />
+          <Route path="/" component={HomePage} exact={true} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
